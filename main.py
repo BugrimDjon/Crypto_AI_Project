44cfd0e9560702fd10b3489055ee9684ad8d2bd3
Евгений Бугрим <bugrimdjon@gmail.com>
2025-06-22 14:10:21 +0300

    отмена огрегации минутных таймфреймов в более старшие, так как применяем агрегайию "на лету"

--- main.py
+++ main.py
@@ -129,10 +129,10 @@ while 1:
             int(time_for_update["current_time_on_the_exchange"]),
             True,
         )
-        agregate_table(Coins.FET)
-        run_servis.calculation_of_indicators(
-            Coins.FET
-        )  # расчет индикаторов по всем таймфреймам
+        # agregate_table(Coins.FET)
+        # run_servis.calculation_of_indicators(
+        #     Coins.FET
+        # )  # расчет индикаторов по всем таймфреймам
     elif button==4:
         # run_servis.ai_expirement_predictions()
         
